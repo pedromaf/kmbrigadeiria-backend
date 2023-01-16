70e8a373f421f964f46fecc6a81493f0ac9423d8
Pedro Marinho <pmaf@ic.ufal.br>
2023-01-16 00:17:23 -0300

    Configuring initial files of user authentication project.

---
+++ KMBrigadeiria.Authentication/Data/AuthDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KMBrigadeiria.Authentication.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> opt) : base(opt)
+        {
+        
+        }
+    }
+}
---
+++ KMBrigadeiria.Authentication/Program.cs
@@ -0,0 +1,43 @@
+using KMBrigadeiria.Authentication.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+#region DB Configuration
+string mySQLConnectionString = builder.Configuration.GetConnectionString("KMBrigadeiriaConnection");
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 30));
+
+builder.Services.AddDbContext<AuthDbContext>(opts => opts.UseLazyLoadingProxies().UseMySql(mySQLConnectionString, serverVersion));
+
+builder.Services.AddIdentity<IdentityUser<int>, IdentityRole<int>>().AddEntityFrameworkStores<AuthDbContext>();
+#endregion
+
+#region AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+#endregion
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseCors(c =>
+{
+    c.AllowAnyHeader();
+    c.AllowAnyMethod();
+    c.AllowAnyOrigin();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
